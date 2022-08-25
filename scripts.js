f0a187a1da401370f8c80d20bac0424f6b87032c
andrew.poyntz <andrew.poyntz@metoffice.gov.uk>
2022-08-25 13:39:36 +0100

    initial checkin showing difference between current and more smoothed polygons

---
+++ scripts.js
@@ -0,0 +1,81 @@
+const LAT_LNG = [53.905, -3.2];
+const TILE_URL = 'https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png';
+let map;
+let freeDraw;
+eventFired = false;
+const polys = L.featureGroup()
+$(document).ready(function() {
+	const sliderCookie = Cookies.get('slider')
+	const sliderValue = (sliderCookie) ? parseFloat(sliderCookie) : 1;
+
+	$('#slider').slider({
+		min:0,
+		max:3,
+		step:0.1,
+		value:sliderValue,
+		change: function( event, ui ) {
+			Cookies.set('slider', ui.value);
+			location.reload();
+		}
+	});
+	if ($('#smoothing').is(':checked')){
+		$('#showBoth').show();
+	}
+	$('#smoothing').change(function () {
+		if ($(this).is(':checked')) {
+			$('#showBoth').show();
+		} else {
+			$('#showBoth').hide();
+		}
+	});
+	$('#clear').click(function() {
+		polys.clearLayers();
+
+	})
+	map = new L.Map(document.querySelector('section.map'), { doubleClickZoom: false }).setView(LAT_LNG, 6);
+	L.tileLayer(TILE_URL).addTo(map);
+	polys.addTo(map);
+	addFreedraw(sliderValue);
+	freeDraw.on('markers', freedrawEvent)
+
+})
+addFreedraw = function (simplfyFactor) {
+	freeDraw = new FreeDraw({ mode: FreeDraw.ALL, simplifyFactor:simplfyFactor});
+	map.addLayer(freeDraw);
+}
+freedrawEvent = function(event) {
+	if (event.eventType === 'create'){
+		drawPoly(event.latLngs);
+		freeDraw.clear();
+	}
+}
+drawPoly = function (latLngs) {
+	// console.log('create poly with', latLngs)
+
+	const polygon = L.polygon(latLngs);
+	// console.log(polygon.toGeoJSON())
+
+	if ($('#smoothing').is(':checked'))  {
+		drawSmoothed(polygon);
+		if ($('#both').is(':checked')) {
+			drawNormal(polygon, latLngs);
+		}
+	} else {
+		drawNormal(polygon, latLngs)
+	}
+
+}
+drawNormal = function (polygon, latLngs){
+	polys.addLayer(polygon);
+	for (let i = 0; i < latLngs[0].length; i++) {
+		polys.addLayer(L.circleMarker(latLngs[0][i], {radius: 2}));
+	}
+}
+drawSmoothed = function (polygon) {
+	const smoothedLatLngs = (turf.flip(turf.polygonSmooth(polygon.toGeoJSON(), {iterations:3}))).features[0].geometry.coordinates;
+	// console.log(smoothedLatLngs);
+	polys.addLayer(L.polygon(smoothedLatLngs, {color:'green'}))
+	for (let i = 0; i < smoothedLatLngs[0].length; i++) {
+		polys.addLayer(L.circleMarker(smoothedLatLngs[0][i], {radius: 2, color: 'green'}));
+	}
+}
